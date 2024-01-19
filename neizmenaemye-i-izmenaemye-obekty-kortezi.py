ff15c385f95d4ff1504b4f11b1b8ff63dd480def
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-01-20 01:18:45 +0800

    neizmenaemye-i-izmenaemye-obekty-kortezi

--- neizmenaemye-i-izmenaemye-obekty-kortezi.py
+++ neizmenaemye-i-izmenaemye-obekty-kortezi.py
@@ -1,11 +1,11 @@
-immutable_var = (True, 'Cat', 156, 2)
-print('Immutable tuple: ', immutable_var)
+immutable_var = (True, 'Cat', 156, 2)  # Создал кортеж
+print('Immutable tuple: ', immutable_var)  # Вывел котреж
 # immutable_var[1] = 'Dog'  Данная операция вызовет ошибку,так как кортеж является неизменяемым объектом
-mutable_list = [True, 'Bird', 852, -56]
-print('Mutable list: ', mutable_list)
-mutable_list[0] = False
-mutable_list[1] = 'Airbus'
-mutable_list[3] = 37
-mutable_list.append('Car')
-mutable_list.remove(852)
-print('Modified mutable list: ', mutable_list)
+mutable_list = [True, 'Bird', 852, -56]  # Создал список
+print('Mutable list: ', mutable_list)  # Вывел список
+mutable_list[0] = False  # Изменил элемент в списке
+mutable_list[1] = 'Airbus'  # Изменил элемент в списке
+mutable_list[3] = 37  # Изменил элемент в списке
+mutable_list.append('Car')  # Добавил элемент в список
+mutable_list.remove(852)  # Удалил элемент из списка
+print('Modified mutable list: ', mutable_list)  # Вывел измененный список
