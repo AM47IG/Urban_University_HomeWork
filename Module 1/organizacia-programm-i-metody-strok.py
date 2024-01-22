09356b18c958fff80f016eaa69a2e3305b9f778e
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-01-22 15:58:28 +0800

    qwe
    qwe

--- Module 1/organizacia-programm-i-metody-strok.py
+++
@@ -1,7 +0,0 @@
-my_string = 'Что я делал прошлым летом'  # Задал переменную
-print('String length: ', len(my_string))  # Вывел длину строки
-print(my_string.upper())  # Вывел строку в верхнем регистре
-print(my_string.lower())  # Вывел строку в нижнем регистре
-print(my_string.replace(' ', ''))  # Вывел строку без пробелов
-print(my_string[0])  # Вывел первый символ
-print(my_string[-1])  # Вывел последний символ
--- Module 1/stroki-i-indeksacia-strok.py
+++
@@ -1,8 +0,0 @@
-my_string = 'Полураспада период'  # Задал переменную
-print('First character:', my_string[0])  # Вывел первый символ
-print('Last character:', my_string[-1])  # Вывел последний символ
-print('Substring:', my_string[2:4])  # Вывел с третьего по пятый символ
-print('Length:', len(my_string))  # Вывел количество символов
-another_string = ': Начало'  # Задал другую переменную
-new_string = my_string + another_string  # Соединил две строки в новую
-print('Concatenated string:', new_string)  # Вывел новую строку
