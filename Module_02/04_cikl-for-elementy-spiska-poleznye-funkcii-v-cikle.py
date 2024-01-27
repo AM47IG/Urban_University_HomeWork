b99d210d6775b4215584feb4856182ff2256ff63
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-01-28 00:36:15 +0800

    cikl-for-elementy-spiska-poleznye-funkcii-v-cikle

---
+++ Module_02/04_cikl-for-elementy-spiska-poleznye-funkcii-v-cikle.py
@@ -0,0 +1,6 @@
+my_car = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+cars_count = 0
+for car in my_car:
+    print('Я езжу на автомобиле марки', car)
+    cars_count += 10
+print('cars_count: ', cars_count)
