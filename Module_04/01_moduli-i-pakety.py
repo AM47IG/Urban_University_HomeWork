2725a8499d3c01bb17239cc514e026f2eb5f3077
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-02-12 18:59:51 +0800

    moduli-i-pakety

---
+++ Module_04/01_moduli-i-pakety.py
@@ -0,0 +1,5 @@
+import module_for_01 as mf
+
+
+mf.function_01()
+mf.function_02('Hello, World!')
---
+++ Module_04/module_for_01.py
@@ -0,0 +1,6 @@
+def function_01():
+    print('Hello, World!')
+
+
+def function_02(a):
+    print(a)
