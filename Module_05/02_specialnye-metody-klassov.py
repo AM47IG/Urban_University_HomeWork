2c1043292db2ceef6796d77ddae61b282bb02e4d
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-02-21 10:42:26 +0800

    Add files via upload

---
+++ Module_05/02_specialnye-metody-klassov.py
@@ -0,0 +1,14 @@
+class House:
+    
+    
+    def __init__(self):
+        self.numberOfFloors = 0
+    
+    
+    def setNewNumberOfFloors(self, floors):
+        self.numberOfFloors = floors
+        print('numberOfFloors:', self.numberOfFloors)
+
+
+house_1 = House()
+house_1.setNewNumberOfFloors(25)
