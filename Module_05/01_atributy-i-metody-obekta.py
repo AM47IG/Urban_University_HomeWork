9d33d3b257234b4cf6cbc2bd071a091088c64269
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-02-23 11:05:38 +0800

    Отступы

--- Module_05/01_atributy-i-metody-obekta.py
+++ Module_05/01_atributy-i-metody-obekta.py
@@ -1,22 +1,21 @@
 class House:
-	
-	
-	def __init__(self, floors=10):
-		self.floors = floors
-		self.current_floor = 1
-	
-	
-	def elevator_on_floor(self, need_a_floor):
-	    print ('Ожидание лифта на этаже:', self.current_floor)
-	    print ('Лифт прибыл')
-	    while self.current_floor != need_a_floor:
-	        print ('Этаж:', self.current_floor)
-	        if self.current_floor > need_a_floor:
-	            self.current_floor -= 1
-	        if self.current_floor < need_a_floor:
-	            self.current_floor += 1
-	    print('Вы на этаже:', self.current_floor)
-            print()
+
+    def __init__(self, floors=10):
+        self.floors = floors
+        self.current_floor = 1
+
+    def elevator_on_floor(self, need_a_floor):
+        print('Ожидание лифта на этаже:', self.current_floor)
+        print('Лифт прибыл')
+        while self.current_floor != need_a_floor:
+            print('Этаж:', self.current_floor)
+            if self.current_floor > need_a_floor:
+                self.current_floor -= 1
+            if self.current_floor < need_a_floor:
+                self.current_floor += 1
+        print('Вы на этаже:', self.current_floor)
+        print()
+
 
 house_1 = House(int(input('Количество этажей:')))
 while True:
--- Module_05/02_specialnye-metody-klassov.py
+++ Module_05/02_specialnye-metody-klassov.py
@@ -1,10 +1,8 @@
 class House:
-    
-    
+
     def __init__(self):
         self.numberOfFloors = 0
-    
-    
+
     def setNewNumberOfFloors(self, floors):
         self.numberOfFloors = floors
         print('numberOfFloors:', self.numberOfFloors)
