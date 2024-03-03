aafa847d4b56886b4f0ced30e41b16ad48dfa44b
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-03-04 01:43:02 +0800

    mnozestvennoe-nasledovanie

---
+++ Module_06/02_mnozestvennoe-nasledovanie.py
@@ -0,0 +1,70 @@
+class Vehicle:
+
+    def __init__(self, vehicle_type='none', *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.vehicle_type = vehicle_type
+
+    def __str__(self):
+        return 'Тип транспорта: {}.'.format(self.vehicle_type)
+
+
+class Car:
+
+    def __init__(self, price=1_000_000, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.price = price
+        self.horse_powers = 125
+        self.cnt_turbo = 0
+
+    def turbo(self):
+        self.cnt_turbo += 1
+        if self.cnt_turbo < 4:
+            self.price += 1_000_000
+            self.horse_powers += 125
+            print('Тачка на прокачку!!!')
+        elif self.cnt_turbo < 5:
+            print('Ваш автомобиль уже очень крут! Остановись!')
+
+    def __str__(self):
+        return 'Цена автомобиля {} {} - {} руб., количество лошадиных сил {}\n'.format(
+            self.__class__.__name__, self.name, self.price, self.horse_powers)
+
+
+class Nissan(Vehicle, Car):
+
+    def __init__(self, name, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = name
+
+    def __str__(self):
+        return super().__str__() + ' ' + Car.__str__(self)
+
+    def __del__(self):
+        print('{} {} {} разбился и больше не существует :(\n'.format(
+            self.vehicle_type, self.__class__.__name__, self.name))
+
+
+almera = Nissan(name='Almera', price=1_250_000, vehicle_type='Автомобиль')
+print(almera.vehicle_type, almera.price)
+
+print(almera)
+almera.turbo()
+print(almera)
+almera.turbo()
+print(almera)
+almera.turbo()
+print(almera)
+almera.turbo()
+print(almera)
+almera.turbo()
+print(almera)
+almera.turbo()
+print(almera)
+
+del almera
+
+fugo = Nissan(name='Fugo', vehicle_type='Автомобиль бизнес класса', price=5_000_000)
+fugo.horse_powers = 350
+print(fugo)
+fugo.turbo()
+print(fugo)
