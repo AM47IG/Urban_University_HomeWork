15eade446694fa98a0c8117434ed3784a98f2b51
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-03-09 01:31:37 +0800

    Update 01_rezimy-otkrytia-fajlov.py

--- Module_7/01_rezimy-otkrytia-fajlov.py
+++ Module_7/01_rezimy-otkrytia-fajlov.py
@@ -4,4 +4,4 @@ file_name = 'my_soul_is_dark.txt'
 file = open(file_name, mode='r', encoding='utf8')
 file_content = file.read()
 pp(file_content)
-file.close()
+file.close()  # Порядок действий как в задании :)
