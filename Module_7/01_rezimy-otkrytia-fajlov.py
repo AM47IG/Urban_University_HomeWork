dc5202427ac5732ef32bc877373f54d6fa1a3759
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-03-09 01:27:44 +0800

    01_rezimy-otkrytia-fajlov

--- Module_7/01_rezimy-otkrytia-fajlov.py
+++ Module_7/01_rezimy-otkrytia-fajlov.py
@@ -1,6 +1,6 @@
 from pprint import pp
 
-file_name = 'text_file.txt'
+file_name = 'my_soul_is_dark.txt'
 file = open(file_name, mode='r', encoding='utf8')
 file_content = file.read()
 pp(file_content)
