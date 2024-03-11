63578a26165f1256684baf3638969123331360d9
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-03-12 01:57:13 +0800

    02_operator-with.py

--- Module_07/02_operator-with.py
+++ Module_07/02_operator-with.py
@@ -1,6 +1,10 @@
 file_name = 'my_soul_is_dark.txt'
 with open(file_name, mode='r', encoding='utf8') as file:
-    for line in file:
+    line = True
+    while line:
+        line = file.readline()
         print(line, end='')
 
-print(file.closed)
+with open(file_name, mode='r', encoding='utf8') as file:
+    for line in file:
+        print(line, end='')
