8dbd657843caa117e47e20bc8f96f47bb12fb22a
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-03-15 02:37:33 +0800

    Update 03_formatirovanie-strok.py

--- Module_07/03_formatirovanie-strok.py
+++ Module_07/03_formatirovanie-strok.py
@@ -6,6 +6,8 @@ class Team:
     quantity_of_all_members = 0
     cnt_welcome_to_the_team = 0
     cnt_end_game = 0
+    solved_tasks_total = 0
+    time_total = 0
 
     def __init__(self, name, quantity_of_members):
         Team.quantity_of_team += 1
@@ -13,8 +15,8 @@ class Team:
         self.name = name
         self.quantity_of_members = quantity_of_members
         self.total_score = 0
-        self.count_of_solved_task = 0
-        self.count_of_task = -1
+        self.count_of_solved_tasks = 0
+        self.count_of_tasks = -1
         self.time = 0
 
 
@@ -25,32 +27,39 @@ class Team:
         if Team.cnt_welcome_to_the_team == Team.quantity_of_team:
             print('Итого сегодня в командах участников: %s!' % Team.quantity_of_all_members, '\n')
 
-    def solved_the_task(self):
+    def solved_the_tasks(self):
         time = randint(700, 4000)
         if time > 3600:
             print('Команда {} не решила задачу!'.format(self.name))
-            self.count_of_task += 1
+            self.count_of_tasks += 1
             return None
         score = 6 - 3600 // time
         self.total_score += score
         self.time += time
-        self.count_of_solved_task += 1
-        self.count_of_task += 1
+        Team.time_total += time
+        self.count_of_solved_tasks += 1
+        self.count_of_tasks += 1
+        Team.solved_tasks_total += 1
         print('Команда {} решила задачу и получает за это {} очков!'.format(self.name, score))
 
     def act(self):
-        if self.count_of_task == -1:
+        if self.count_of_tasks == -1:
             self.welcome_to_the_team()
-            self.count_of_task += 1
-        elif self.count_of_task < 5:
-            self.solved_the_task()
-        elif self.count_of_task == 5:
+            self.count_of_tasks += 1
+        elif self.count_of_tasks < 20:
+            self.solved_the_tasks()
+        else:
             print('\nКоманда {} закончила игру со счётом {}! \nВсе задачи решены за {} сек.'.format(
                 self.name, self.total_score, self.time))
             Team.cnt_end_game += 1
 
-    def __eq__(self):
-        return self.count_of_solved_task
+    def __le__(self, other):
+        if self.count_of_solved_tasks < other.count_of_solved_tasks:
+            print(f'Победа команды {other.name}')
+        elif other.count_of_solved_tasks < self.count_of_solved_tasks:
+            print(f'Победа команды {self.name}')
+        else:
+            print(f'Команды {self.name} и {other.name} делят победу!')
 
 
 print(f'{'Игра началась':-^40}')
@@ -61,8 +70,13 @@ team2.act()
 while Team.cnt_end_game != Team.quantity_of_team:
     team1.act()
     team2.act()
+    print()
 
 print(f'{'Игра окончена':-^40}')
-print(f'\nКоманды решили {team1.count_of_solved_task} и {team2.count_of_solved_task} задач.')
-win_team = max(team1, team2)
-print(win_team)
+print(f'\nКоманды решили {team1.count_of_solved_tasks} и {team2.count_of_solved_tasks} задач.\n')
+
+team1 <= team2
+
+tasks_total = Team.solved_tasks_total
+time_avg = Team.time_total / tasks_total
+print(f'\nСегодня было решенно {tasks_total} задач, в среднем по {time_avg:.1f} секунды на задачу!')
