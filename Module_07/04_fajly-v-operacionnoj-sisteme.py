96995995b0004fddefcd8683d4ce9c4d5dba2a99
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-03-16 11:46:46 +0800

    Update 04_fajly-v-operacionnoj-sisteme.py

--- Module_07/04_fajly-v-operacionnoj-sisteme.py
+++ Module_07/04_fajly-v-operacionnoj-sisteme.py
@@ -11,5 +11,9 @@ for root, dirs, files in os.walk(directory):
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
         filesize = os.path.getsize(filepath)
         parent_dir = os.path.dirname(filepath)
-        print(f'Обнаружен файл: {file}, \nПуть: {filepath}, \nРазмер: {filesize} байт, \nВремя изменения: {formatted_time}, \nРодительская директория: {parent_dir}')
         print('*' * 20)
+        print(f'Обнаружен файл: "{file}",\n'
+              f'Путь: {filepath},\n'
+              f'Размер: {filesize} байт,\n'
+              f'Время изменения: {formatted_time},\n'
+              f'Родительская директория: {parent_dir}', '\n')
