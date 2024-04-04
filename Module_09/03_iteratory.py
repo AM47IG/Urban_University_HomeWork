cd62b772aac6590906979d829ef1677720e045c2
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-04-04 16:48:52 +0800

    Update 03_iteratory.py

--- Module_09/03_iteratory.py
+++ Module_09/03_iteratory.py
@@ -1,26 +1,21 @@
 class EvenNumbers:
 
     def __init__(self, start=0, end=1):
-        self.start = start
+        self.start = start if start % 2 == 0 else start + 1
         self.end = end
-        self.i = 0
 
     def __iter__(self):
-        self.i = 0
-        if self.start % 2 != 0:  # Главное начать с четного :)
-            self.start += 1
         return self
 
     def __next__(self):
-        self.i += 1
-        if self.i > 1:
-            self.start += 2  # До исключения, потому что end не включительно!
-            if self.start >= self.end:
-                raise StopIteration()
-        return self.start
+        result = self.start
+        self.start += 2
+        if self.start - 2 >= self.end:
+            raise StopIteration()
+        return result
 
 
-en = EvenNumbers(start=-9, end=26)
+en = EvenNumbers(start=-9, end=27)
 for i in en:
     print(i)
 print(1 in en)  # False быстро
