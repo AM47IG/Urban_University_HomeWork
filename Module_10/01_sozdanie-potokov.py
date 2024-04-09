95d2bd02a95168454e235429ad0c4656a56047d0
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-04-09 15:15:10 +0800

    Update 01_sozdanie-potokov.py

--- Module_10/01_sozdanie-potokov.py
+++ Module_10/01_sozdanie-potokov.py
@@ -2,13 +2,13 @@ import time
 from threading import Thread
 
 
-def output(tpl=None):
+def output(tpl):
     for el in tpl:
         print(el, flush=True)
         time.sleep(1)
 
 
-thread = Thread(target=output, kwargs=dict(tpl=(range(1, 11))))
+thread = Thread(target=output, args=(range(1, 11), ))
 thread.start()
 time.sleep(0.001)
 output((chr(i) for i in range(ord('a'), ord('a') + 10)))
