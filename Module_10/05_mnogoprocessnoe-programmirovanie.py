71b1f36bdd9ecf8be0b2a592967e88556df3df84
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-04-17 15:37:34 +0800

    05_mnogoprocessnoe-programmirovanie.py

---
+++ Module_10/05_mnogoprocessnoe-programmirovanie.py
@@ -0,0 +1,57 @@
+import time
+from multiprocessing import Process, Pipe
+
+
+class WarehouseManager:
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.data = {}
+
+    def process_request(self, request, conn):
+        product, process, value = request
+        data = conn.recv()
+        if process == "receipt":
+            data[product] = data.get(product, 0) + value
+            print(f'В data: {product} увеличено на {value}')
+        elif process == "shipment" and data.get(product, False) and data[product] >= value:
+            data[product] -= value
+            print(f'В data: {product} уменьшено на {value}')
+        else:
+            print(f'В data: нет {product} или оно меньше {value}')
+        conn.send(data)
+        conn.close()
+        return
+
+    def run(self, requests):
+        procs, pipes = [], []
+        for request in requests:
+            parent_conn, child_conn = Pipe()
+            procs.append(Process(target=self.process_request, args=(request, child_conn)))
+            pipes.append(parent_conn)
+        [proc.start() for proc in procs]
+        for conn in pipes:
+            conn.send(self.data)
+            self.data = conn.recv()
+            conn.close()
+        [proc.join() for proc in procs]
+
+
+# Создаем менеджера склада
+manager = WarehouseManager()
+
+# Множество запросов на изменение данных о складских запасах
+requests = [
+    ("product1", "receipt", 100),
+    ("product2", "receipt", 150),
+    ("product1", "shipment", 30),
+    ("product3", "receipt", 200),
+    ("product2", "shipment", 50)
+]
+
+if __name__ == '__main__':
+    # Запускаем обработку запросов
+    manager.run(requests)
+
+    # Выводим обновленные данные о складских запасах
+    print(manager.data)
