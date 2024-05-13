3b33ae8b5691452318c2b87e1523002ffcee5c17
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-05-13 15:27:57 +0800

    logging

---
+++ Module_12/RequestsLogger/main.py
@@ -0,0 +1,25 @@
+import requests as rq
+import logging.config
+from log_settings import log_config
+
+logging.config.dictConfig(log_config)
+log_success = logging.getLogger('success')
+log_bad = logging.getLogger('bad')
+log_blocked = logging.getLogger('blocked')
+
+sites = ['https://www.youtube.com/', 'https://instagram.com', 'https://wikipedia.org', 'https://yahoo.com',
+         'https://yandex.ru', 'https://whatsapp.com', 'https://twitter.com', 'https://amazon.com', 'https://tiktok.com',
+         'https://www.ozon.ru']
+
+for site in sites:
+    try:
+        response = rq.get(site, timeout=3)
+    except rq.exceptions.ConnectionError:
+        log_blocked.error(f'\'{site}\', NO CONNECTION')
+    except rq.exceptions.ReadTimeout:
+        log_blocked.error(f'\'{site}\', READ TIMEOUT')
+    else:
+        if response.status_code == 200:
+            log_success.info(f'\'{site}\', response - 200')
+        else:
+            log_bad.warning(f'\'{site}\', response - {response.status_code}')
