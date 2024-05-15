12ad2fbb27e485cd8a645a10d2b6145d2e193201
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-05-15 11:35:18 +0800

    zanovo

--- Module_01/01_stroki-i-indeksacia-strok.py
+++ Module_01/01_stroki-i-indeksacia-strok.py
@@ -1,8 +1,8 @@
-my_string = 'Полураспада период'  # Задал переменную
-print('First character:', my_string[0])  # Вывел первый символ
-print('Last character:', my_string[-1])  # Вывел последний символ
-print('Substring:', my_string[2:4])  # Вывел с третьего по пятый символ
-print('Length:', len(my_string))  # Вывел количество символов
-another_string = ': Начало'  # Задал другую переменную
-new_string = my_string + another_string  # Соединил две строки в новую
-print('Concatenated string:', new_string)  # Вывел новую строку
+my_string = 'этож моя строка'   # Задал переменную
+print('Первый символ:', my_string[0])  # Вывел первый символ
+print('Последний символ:', my_string[-1])  # Вывел последний символ
+print('Подстрока:', my_string[2:4])  # Вывел с третьего по пятый СИМВОЛ (не включительно)
+print('Строка наоборот:', my_string[::-1])  # Вывел строку наоборот
+new_string = 'этож новая строка'  # Задал новую переменную
+concatenated_string = new_string + ' ' + my_string  # Соединил две строки
+print('Соединение строк:', concatenated_string)  # Вывел новую строку
