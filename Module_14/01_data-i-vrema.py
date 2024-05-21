823a09dee9902c2123755d428479cf83cf66967b
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-05-21 21:05:56 +0800

    Update 01_data-i-vrema.py

--- Module_14/01_data-i-vrema.py
+++ Module_14/01_data-i-vrema.py
@@ -1,28 +1,28 @@
 from datetime import datetime
 
-SEASON = {'Summer': (6, 7, 8),
-          'Autumn': (9, 10, 11),
-          'Winter': (12, 1, 2),
-          'Spring': (3, 4, 5),
+SEASON = {(6, 7, 8): 'Summer',
+          (9, 10, 11): 'Autumn',
+          (12, 1, 2): 'Winter',
+          (3, 4, 5): 'Spring',
           }
 
-TIME_OF_DAY = {'Morning': (6, 7, 8, 9, 10, 11),
-               'Day': (12, 13, 14, 15, 16, 17),
-               'Evening': (18, 19, 20, 21, 22, 23),
-               'Night': (0, 1, 2, 3, 4, 5)
+TIME_OF_DAY = {(6, 7, 8, 9, 10, 11): 'Morning',
+               (12, 13, 14, 15, 16, 17): 'Day',
+               (18, 19, 20, 21, 22, 23): 'Evening',
+               (0, 1, 2, 3, 4, 5): 'Night'
                }
 
 
 class SuperDate(datetime):
     def get_season(self):
-        for k, v in SEASON.items():
-            if self.month in v:
-                return k
+        for k in SEASON:
+            if self.month in k:
+                return SEASON[k]
 
     def get_time_of_day(self):
-        for k, v in TIME_OF_DAY.items():
-            if self.hour in v:
-                return k
+        for k in TIME_OF_DAY:
+            if self.hour in k:
+                return TIME_OF_DAY[k]
 
 
 a = SuperDate(2024, 2, 22, 12)
