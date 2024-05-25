56ae5bfa0258f49afec4ba1653ec7a7cfdd4ac19
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-05-25 17:30:32 +0800

    04_json-fajly

---
+++ Module_14/EmployeesTask-master/main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import json
+
+
+def employees_rewrite(sort_type):
+    with open('employees.json', mode='r', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+    for k in data['employees'][0]:
+        if k.lower() == sort_type.lower():
+            valid_sort_type = k
+            break
+    else:
+        raise ValueError('Bad key for sorting')
+    sort_data = sorted(data['employees'], key=lambda x: x[valid_sort_type])
+    with open(f'employees_{sort_type}_sorted.json', mode='w+', encoding='utf-8') as out_file:
+        json.dump(sort_data, out_file, indent=4)
+
+
+employees_rewrite('department')
