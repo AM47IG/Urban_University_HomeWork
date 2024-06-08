f9d2d0628dc2c21394ccbbb8dd0239c9f0de875b
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-06-08 19:54:10 +0800

    01_sozdanie-obektov-obrabotka-izobrazenij.py

--- Module_15/01_sozdanie-obektov-obrabotka-izobrazenij.py
+++ Module_15/01_sozdanie-obektov-obrabotka-izobrazenij.py
@@ -15,9 +15,9 @@ for i in range(0, 450):
     image[0:225, i:i+1] = i // 2, i // 2, i // 2
 
 for i in (-90, -50, 50, 90):
-    cv2.line(image, (cntr + i, cntr), (cntr + i, cntr - 50), (170, 0, 0), thickness=5)
+    cv2.line(image, (cntr + i, cntr), (cntr + i, cntr - 100), (170, 0, 0), thickness=5)
 
-for i in (((135, 175), (175, 175)), ((275, 175), (315, 175))):
+for i in (((135, 125), (175, 125)), ((275, 125), (315, 125))):
     cv2.line(image, *i, (0, 0, 170), thickness=5)
 
 cv2.circle(image, (cntr, cntr), 200, (255, 255, 255), thickness=5)
