9d66d2fac6cde210e2591692db1ce7bffea8f50e
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-06-12 23:40:32 +0800

    03_raspoznavanie-obektov.py

--- Module_15/03_raspoznavanie-obektov/03_raspoznavanie-obektov.py
+++ Module_15/03_raspoznavanie-obektov/03_raspoznavanie-obektov.py
@@ -1,16 +1,36 @@
+import time
+
 import cv2
+import numpy as np
 
 
 cap = cv2.VideoCapture('sample.mp4')
+# cap = cv2.VideoCapture(0)
 eyes = cv2.CascadeClassifier('eyes.xml')
+mask = np.zeros(cap.read()[1].shape[:2], dtype=np.uint8)
 
 while True:
     success, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    results = eyes.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=3)
-    for (x, y, w, h) in results:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), thickness=3)
-
-    cv2.imshow('Result', img)
+    find_eyes = eyes.detectMultiScale(gray, scaleFactor=3.5, minNeighbors=10)
+    gray = cv2.GaussianBlur(gray, (1, 91), 0, sigmaY=91)
+    if len(find_eyes) > 1:
+        mask[:] = 0
+        cur = []
+        for x, y, w, h in find_eyes:
+            # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), thickness=3)
+            cur.append((x, y))
+            cur.append((x + w, y + h))
+        eyes_xy = ((min(cur)[0] - 50, min(cur, key=lambda el: el[1])[1]),
+                   (max(cur)[0] + 50, max(cur, key=lambda el: el[1])[1]))
+        cv2.rectangle(mask, *eyes_xy, (255, ), thickness=-1)
+    gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+    gray = cv2.bitwise_and(gray, gray, mask=mask)
+    img = cv2.bitwise_and(img, img, mask=cv2.bitwise_not(mask))
+    result = cv2.bitwise_or(img, gray)
+    # cv2.imshow('Mask', mask)
+    # cv2.imshow('Gray', gray)
+    # cv2.imshow('IMG', img)
+    cv2.imshow('Result', result)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
