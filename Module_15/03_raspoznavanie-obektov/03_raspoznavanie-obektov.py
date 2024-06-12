e2e03938a2dcaf8abb8a2093e08e6356d19923c6
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-06-12 23:53:37 +0800

    03_raspoznavanie-obektov.py

--- Module_15/03_raspoznavanie-obektov/03_raspoznavanie-obektov.py
+++ Module_15/03_raspoznavanie-obektov/03_raspoznavanie-obektov.py
@@ -14,12 +14,12 @@ while True:
     gray = cv2.GaussianBlur(gray, (1, 91), 0, sigmaY=91)
     if len(find_eyes) > 1:
         mask[:] = 0
-        cur = []
+        cur = [[], []]
         for x, y, w, h in find_eyes:
             # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), thickness=3)
-            cur.extend([(x, y), (x + w, y + h)])
-        eyes_xy = ((min(cur)[0] - 50, min(cur, key=lambda el: el[1])[1]),
-                   (max(cur)[0] + 50, max(cur, key=lambda el: el[1])[1]))
+            cur[0].extend([x, x + w])
+            cur[1].extend([y, y + h])
+        eyes_xy = ((min(cur[0]) - 50, min(cur[1])), (max(cur[0]) + 50, max(cur[1])))
         cv2.rectangle(mask, *eyes_xy, (255, ), thickness=-1)
     gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
     gray = cv2.bitwise_and(gray, gray, mask=mask)
