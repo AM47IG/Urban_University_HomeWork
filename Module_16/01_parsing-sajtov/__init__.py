406652d9e6d89ce0fb478f1a2ce86a9fed98dd1d
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-06-16 16:45:53 +0800

    01_parsing-sajtov

---
+++ Module_16/01_parsing-sajtov/__init__.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import time
+import datetime
+import csv
+from decimal import Decimal as D
+
+
+def write_cmc_top():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    url = 'https://coinmarketcap.com/ru/'
+    driver.get(url)
+    driver.execute_script("window.scroll({ top: document.body.scrollHeight, behavior: 'smooth' });")
+    time.sleep(10)
+    soup = BeautifulSoup(driver.page_source, features='html.parser')
+    list_of_name, list_of_mc = [], []
+    for coin in soup.find_all('tr')[1:]:
+        list_of_name.append((coin.find_next('p', class_='sc-71024e3e-0 ehyBa-d').get_text().split()[0]))
+        list_of_mc.append(coin.find_next('span', class_='sc-11478e5d-1 hwOFkt').get_text()[1:])
+    capitalization_of_top_100 = sum(map(lambda x: int(x.replace(',', '')), list_of_mc))
+    list_of_mp = [D(100) / D(capitalization_of_top_100) * D(mc) for mc in
+                  map(lambda x: int(x.replace(',', '')), list_of_mc)]
+    with open(f'{datetime.datetime.now().strftime('%H.%M %d.%m.%Y')}.csv', 'w', encoding='utf-8') as output:
+        csv_out = csv.writer(output, delimiter=' ', lineterminator="\r")
+        csv_out.writerow(['Name', 'MC', 'MP'])
+        cnt = 0
+        for name, mc, mp in zip(list_of_name, list_of_mc, list_of_mp):
+            csv_out.writerow([name, mc, f'{mp.quantize(D('1.00'))}%'])
+            cnt += 1
+    print(f'Файл {output.name} записан! Кол-во строк {cnt}')
+
+
+if __name__ == "__main__":
+    write_cmc_top()
