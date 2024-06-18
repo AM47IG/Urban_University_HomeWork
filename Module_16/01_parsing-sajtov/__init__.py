9b323623ec3da4fe955fdce783ccab9731857eab
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-06-18 12:08:25 +0800

    01_parsing-sajtov

--- Module_16/01_parsing-sajtov/__init__.py
+++ Module_16/01_parsing-sajtov/__init__.py
@@ -25,6 +25,8 @@ def write_cmc_top():
     driver.get(url)
     driver.execute_script("setInterval(function(){window.scrollBy({ top: 1024 });}, 500)")
     time.sleep(7)
+
+    assert driver.current_url == 'https://coinmarketcap.com/ru/'
     soup = BeautifulSoup(driver.page_source, features='html.parser')
 
     # Создание необходимых коллекций для дальнейшей записи в файл
@@ -34,20 +36,19 @@ def write_cmc_top():
         list_of_mc.append(coin.find_next('span', class_='sc-11478e5d-1 hwOFkt').get_text()[1:])
     capitalization_of_top_100 = sum(map(lambda x: int(x.replace(',', '')), list_of_mc))
     list_of_mp = [100 / capitalization_of_top_100 * mc for mc in map(lambda x: int(x.replace(',', '')), list_of_mc)]
-    if len(set(list_of_name)) != 100 or len(list_of_name) != 100:
-        raise Exception(f'Ошибка! Считано {len(set(list_of_name))} строк вместо 100.')
+
+    assert len(set(list_of_name)) == 100 and len(list_of_name) == 100, \
+        f'Ошибка! Считано {len(set(list_of_name))} строк вместо 100.'
 
     # Запись данных в файл
     with open(f'{datetime.datetime.now().strftime('%H.%M %d.%m.%Y')}.csv', 'w', encoding='utf-8') as output:
         csv_out = csv.writer(output, delimiter=' ', lineterminator="\r")
         csv_out.writerow(['Name', 'MC(RUB)', 'MP'])
-        cnt = 0
         for name, mc, mp in zip(list_of_name, list_of_mc, list_of_mp):
             csv_out.writerow([name, mc, f'{round(mp, 2)}%'])
-            cnt += 1
 
     # Успех!
-    print(f'Файл {output.name} записан! Кол-во строк {cnt}')
+    print(f'Файл {output.name} записан!')
 
 
 if __name__ == "__main__":
