e91198969ed5befbfbfc98b29458a5aa0fac4866
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-07-20 16:50:45 +0800

    Pool and Manager

--- Module_10/05_mnogoprocessnoe-programmirovanie.py
+++ Module_10/05_mnogoprocessnoe-programmirovanie.py
@@ -1,52 +1,41 @@
-from multiprocessing import Process, Pipe
+from multiprocessing import Pool, Manager
 
 
 class WarehouseManager:
 
     def __init__(self):
-        self.data = {}
+        self.data = Manager().dict()
 
-    def process_request(self, request, conn):
-        product, process, value = request
-        data = conn.recv()
-        if process == "receipt":
-            data[product] = data.get(product, 0) + value
-            print(f'Склад: {product} увеличено на {value}')
-        elif process == "shipment" and data.get(product, False) and data[product] >= value:
-            data[product] -= value
-            print(f'Склад: {product} уменьшено на {value}')
+    def process_request(self, request):
+        product, operation, amount = request
+        if operation == "receipt":
+            self.data[product] = self.data.get(product, 0) + amount
+            print(f'Склад: {product} увеличено на {amount}')
+        elif operation == "shipment" and self.data.get(product, False) and self.data[product] >= amount:
+            self.data[product] -= amount
+            print(f'Склад: {product} уменьшено на {amount}')
         else:
-            print(f'Склад: {product} отсутствует или его значение меньше {value}')
-        conn.send(data)
-        conn.close()
+            print(f'Склад: {product} отсутствует или его значение меньше {amount}')
 
     def run(self, requests):
-        procs, pipes = [], []
-        for request in requests:
-            parent_conn, child_conn = Pipe()
-            procs.append(Process(target=self.process_request, args=(request, child_conn)))
-            pipes.append(parent_conn)
-        [proc.start() for proc in procs]
-        for conn in pipes:
-            conn.send(self.data)
-            self.data = conn.recv()
-            conn.close()
-        [proc.join() for proc in procs]
+        amount_processes = len(requests)
+        processes = []
+        with Pool(processes=amount_processes) as pool:
+            pool.map(self.process_request, requests)
 
 
-# Создаем менеджера склада
-manager = WarehouseManager()
-
-# Множество запросов на изменение данных о складских запасах
-requests = [
-    ("product1", "receipt", 100),
-    ("product2", "receipt", 150),
-    ("product1", "shipment", 30),
-    ("product3", "receipt", 200),
-    ("product2", "shipment", 50)
-]
 
 if __name__ == '__main__':
+    # Создаем менеджера склада
+    manager = WarehouseManager()
+    # Множество запросов на изменение данных о складских запасах
+    requests = [
+        ("product1", "receipt", 100),
+        ("product2", "receipt", 150),
+        ("product1", "shipment", 30),
+        ("product3", "receipt", 200),
+        ("product2", "shipment", 50)
+    ]
     # Запускаем обработку запросов
     manager.run(requests)
     # Выводим обновленные данные о складских запасах
