003bfb772a2ae6b791fd45b640fa3147f54dfe3b
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-07-25 21:20:24 +0800

    module_16_4

--- Module_16/module_16_4.py
+++ Module_16/module_16_4.py
@@ -7,7 +7,7 @@ users = []
 
 
 class User(BaseModel):
-    id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example=1)]
+    id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example=1)] = None
     username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")]
     age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)]
 
@@ -30,15 +30,19 @@ async def post_user(user: User) -> User:
 @app.put("/user/{user_id}/{username}/{age}")
 async def put_user(user: User) -> User:
     try:
-        users[user.id - 1] = user
+        index = list(map(lambda x: x.id, users)).index(user.id)
+        users[index] = user
         return user
-    except IndexError:
+    except (IndexError, ValueError):
         raise HTTPException(status_code=404, detail="User was not found")
+    except TypeError:
+        raise HTTPException(status_code=404, detail="User id is not specified")
 
 
 @app.delete("/user/{user_id}")
 async def delete_user(user_id: int) -> User:
     try:
-        return users.pop(user_id - 1)
-    except IndexError:
+        index = list(map(lambda x: x.id, users)).index(user_id)
+        return users.pop(index)
+    except (IndexError, ValueError):
         raise HTTPException(status_code=404, detail="User was not found")
